1c304d2afc45f8a4625bcf13678ffd5e56d4bb21
Andrius K <akvara@gmail.com>
2018-01-23 21:55:07 +0200

    echo

--- id-envs.sh
+++ id-envs.sh
@@ -5,3 +5,4 @@ mkdir environments
 cd environments
 virtualenv -p python3 id_backend
 cd ~/CCT/inform-debtor
+echo /home/andrius/CCT/environments/id_backend/bin/
--- lc-envs.sh
+++ lc-envs.sh
@@ -5,3 +5,4 @@ mkdir environments
 cd environments
 virtualenv -p python3 lc_backend
 cd ~/CCT/life-coach
+/home/andrius/CCT/environments/lc_backend/bin/
