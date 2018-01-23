63fcfabc8efffef86895cbb50db3fe1045b68a0d
Andrius K <akvara@gmail.com>
2018-01-23 18:33:07 +0200

    webStormTrial.sh

---
+++ changeHostname.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "" ]; then
+    echo usage: $1 new-host-name
+    exit 1
+fi
+
+host=$(hostname)
+echo changing $host to $1
+sudo sed -i "s/$host/$1/" /etc/hosts
+sudo sed -i "s/$host/$1/" /etc/hostname
---
+++ webStormTrial.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd ~/.WebStorm*
+rm config/eval/WebStorm*evaluation.key
+rm config/options/options.xml
+cd ~/.java/.userPrefs/jetbrains
+rm -rf webstorm
