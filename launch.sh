08bd18f333ccf684580d7d8b19ac343a6c0661cf
Andrius K <akvara@gmail.com>
2018-04-17 19:04:58 +0300

    & at the end

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo sysctl -w vm.max_map_count=262144
-cd ~/CCT/layerv-ccb/ && ./bin/develop.sh
-google-chrome &
 webstorm.sh &
 pycharm &
+cd ~/CCT/layerv-ccb/ && ./bin/develop.sh &
+google-chrome &
