6925847281755de4bd68fae52677679cf8b58914
Andrius K <akvara@gmail.com>
2018-04-30 13:01:56 +0300

    feature

--- git-leave-master.sh
+++ git-leave-master.sh
@@ -1,3 +1,3 @@
 git co master
 git remote update origin --prune
-git branch | grep -v '^[ *]*master$' | grep -v '^[ *]*staging$' | xargs git branch -D
+git branch | grep -v '^[ *]*master$' | grep -v '^[ *]*staging$'| grep -v '^[ *]*feature$' | xargs git branch -D
--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo sysctl -w vm.max_map_count=262144
+slack &
 webstorm.sh &
 pycharm &
-cd ~/CCT/layerv-ccb/ && ./bin/develop.sh &
 google-chrome &
+cd ~/CCT/layerv-ccb/ && ./bin/develop.sh &
