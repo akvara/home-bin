bbfcf3caff017460e495e8ac42b83d49006fd8f7
Andrius K <akvara@gmail.com>
2018-12-31 21:02:27 +0200

    Colors everywhere

--- colors.sh
+++ colors.sh
@@ -3,11 +3,14 @@
 # Set colors
 ESC_SEQ="\x1b["
 COL_RESET=$ESC_SEQ"39;49;00m"
+COL_BLACK=$ESC_SEQ"30;01m"
 COL_RED=$ESC_SEQ"31;01m"
 COL_GREEN=$ESC_SEQ"32;01m"
 COL_GRAY=$ESC_SEQ"33;01m"
 COL_BLUE=$ESC_SEQ"34;01m"
 COL_PINK=$ESC_SEQ"35;01m"
+COL_MAGENTA=$ESC_SEQ"36;01m"
+COL_LIGHT=$ESC_SEQ"37;01m"
 
 check_git_result () {
    if [[ $? -ne 0 ]]; then
