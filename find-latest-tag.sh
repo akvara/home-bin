902f639a88c44487c598c532a530d2abb49b1570
Andrius Kvaraciejus <akvara@gmail.com>
2020-02-22 11:08:26 +0200

    find-latest-tag improved

--- find-latest-tag.sh
+++ find-latest-tag.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/colors.sh
 
+COUNT=1
+MSG="checking out develop"
+message "$MSG"
+git checkout develop
+check_git_result "$MSG"
+
+let "COUNT=COUNT+1"
+MSG="fetching tags"
+message "$MSG"
 git fetch origin develop --tags
+check_git_result "$MSG"
 
+let "COUNT=COUNT+1"
+MSG="calculating tag"
+message "$MSG"
 git tag | node $DIR/find-latest-tag.js $DIR
+check_git_result "$MSG"
